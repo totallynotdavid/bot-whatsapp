9554a89159f246210d1f2ef3fc85295c50df2e2d
David <20960328+totallynotdavid@users.noreply.github.com>
2026-09-23 03:40:58 +0000

    chore: remove legacy JavaScript implementation superseded by the TypeScript rewrite

--- services/message.js
+++
@@ -1,7 +0,0 @@
-const { robotEmoji } = require("../functions/globals");
-
-async function sendMessage(client, message, text) {
-  await client.sendMessage(message.from, `${robotEmoji} ${text}`);
-}
-
-module.exports = { sendMessage };
